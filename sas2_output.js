b928aba3d2681cb73773e2e5d44ba6b8a749ad32
alkhanssae-el <alkhanssae.elorche@gmail.com>
2026-09-25 10:22:10 +0000

    saves

---
+++ sas2_output.js
@@ -0,0 +1,13 @@
+const candidates = []
+console.log(candidates)
+//1. Add a new candidate
+candidates.push({
+	cin: "AB123456",
+	lastName: "Boushaba",
+	firstName: "Soufiane",
+	politicalParty: "Independent",
+	age: 40,
+	voters: []
+})
+console.log(candidates)
+//2. Add several candidates at once
