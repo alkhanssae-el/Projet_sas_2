c021e44c0d59a3cd6a59ab87d275390c34ea77ed
alkhanssae-el <alkhanssae.elorche@gmail.com>
2026-09-26 09:22:00 +0000

    saves

--- sas2_output.js
+++ sas2_output.js
@@ -2,7 +2,7 @@ const candidates = []
 //console.log(candidates)
 //1;2. Add a new candidate
 function candidateAdd(cin, lastName, firstName, politicalParty, age, voters){
-	candidates.push({cin: cin , lastName: lastName, firstName: firstName, politicalParty: politicalParty, age: age, voters: +voters})
+	candidates.push({cin: cin , lastName: lastName, firstName: firstName, politicalParty: politicalParty, age: age, voters: voters})
 }
 candidateAdd("ST012345", "Fassi", "Khadija", "PAM", 31, "[]")
 //console.log(candidates)
